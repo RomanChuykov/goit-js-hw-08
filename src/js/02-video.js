a48f0ae15a5c07143b4383742ffc55919ed4af51
RomanChuykov <95050528+RomanChuykov@users.noreply.github.com>
2023-08-07 21:25:17 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
 
+const iframe = document.querySelector('iframe');
+
+import Player from '@vimeo/player';
+
+
+const player = new Player(iframe, {
+    id: 19231868,
+    width: 640
+});
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+player.on('timeupdate', function() {
+   player.getCurrentTime().then(function(seconds) {
+    // seconds = the current playback position
+    let time=seconds;
+    localStorage.setItem("videoplayer-current-time", time);
+    console.log(time)
+}).catch(function(error) {
+    // an error occurred
+});
+    
+});
+
+
+player.setCurrentTime(parseFloat(localStorage.getItem("videoplayer-current-time"))).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+var  throttle  = require ( 'lodash.throttle' ) ; 
+
+const email=document.querySelector("input[name=email]")
+const form=document.querySelector(".feedback-form");
+const message= document.querySelector("textarea[name=message]")
+let storage='';
+let parsJson;
+if(localStorage.getItem("feedback-form-state")){
+    parsJson=JSON.parse(localStorage.getItem("feedback-form-state"))
+    email.value=parsJson.email;
+    message.value=parsJson.message;
+}
+const  json={
+email:email.value,
+message:message.value,
+};
+form.addEventListener("input", throttle(onInput,500))
+form.addEventListener("submit",onSubmit)
+
+function onInput(){
+    json.email=email.value
+        json.message=message.value
+         console.log(json);
+         storage=JSON.stringify(json);
+         localStorage.setItem("feedback-form-state", storage)
+}
+function onSubmit(e){
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+    console.log('submit')
+}
+
+
+// const dog = JSON.parse(json);
+// const theme = localStorage.getItem("ui-theme");
 
